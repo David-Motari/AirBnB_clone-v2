94b2e8c001dd1ad8b110fc3d52c09feeb39291ff
David Motari <davemotari22@gmail.com>
2022-09-13 20:44:07 +0000

    MySQL setup development

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,9 +5,10 @@
 -- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
 -- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
 -- If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS hbn_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbn_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
