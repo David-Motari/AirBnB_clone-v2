d34c73cd2f9cc67280bf8cad54c9afe0111f41de
laura <rosechebet1997@gmail.com>
2022-09-14 12:41:28 -0700

    change the base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,36 @@
 import uuid
 from datetime import datetime
 
+Base = declarative_base()
 
+
+s = "HBNH_TYPE_STORAGE"
 class BaseModel:
     """A base class for all hbnb models"""
+    if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+        id = Column(
+                String(60),
+                unique=True,
+                nullable=False,
+                primary_key=True,
+                default=str(uuid.uuid4()))
+        created_at = Column(
+                 DateTime,
+                 nullable=False,
+                 default=datetime.utcnow())
+        updated_at = Column(
+                DateTime,
+                nullable=False,
+                default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        cs = "HBNB_TYPE_STORAGE"
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +50,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +61,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary.keys():
+            del dictionary["_sa_instance_state"]
+            models.storage.save()
         return dictionary
