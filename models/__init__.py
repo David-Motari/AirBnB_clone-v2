ff30b68e4e826f88d290051f5c3389a6071de167
laura <rosechebet1997@gmail.com>
2022-09-16 14:51:38 -0700

    fix error

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,10 +9,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-import os
+from os import getenv
 
 
-SELECTED_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
+SELECTED_STORAGE = getenv("HBNB_TYPE_STORAGE")
 if SELECTED_STORAGE == "db":
     storage = DBStorage()
     storage.reload()
