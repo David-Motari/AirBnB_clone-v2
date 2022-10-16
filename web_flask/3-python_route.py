62cb953c38d3254ff8eb6d7bda7cad961f57fe10
David Motari <davemotari22@gmail.com>
2022-10-16 03:39:55 +0000

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+2-python_route
+starts a Flask web application
+"""
+from flask import Flask
+import os
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    returns Hello HBNB!
+    """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    returns HBNB
+    """
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    returns C with variable
+    """
+    text1 = text.replace("_", " ")
+    return 'C {}'.format(text1)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    """
+    returns python with variable
+    """
+    text1 = text.replace("_", " ")
+    return 'Python {}'.format(text1)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
